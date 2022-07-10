2038c0887665220117e7d0159753e763c909f16c
Shelby Trodden <shelby.trodden@gmail.com>
2022-07-10 10:17:15 -0500

    get form to be able to resubmit

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,38 @@
-// User Interface Logic
-window.addEventListener("load", function(event) {
+/ User Interface Logic
+window.addEventListener('load', function () {
+    const form = document.getElementById('roboger');
+    const resetBtn = document.getElementById('reset');
+    const submitButton = document.getElementsByClassName('btn')[0];
 
-    const form = document.getElementById("roboger");
-    const resetBtn =  document.getElementById("reset");
-    
-form.addEventListener("submit", function(event) {
+form.addEventListener('submit', function (event) {
     event.preventDefault();
-    
-    const numInput = parseInt(document.getElementById("number").value);
+
+    const numInput = parseInt(document.getElementById('number').value);
     const result = numIdentify(numInput);
-    
+    submitButton.setAttribute('disabled', 'disabled');
 
     function numIdentify(numInput) {
-        numArray=[];
-        for (let i = 0; i <= numInput; i++){
-            let inputString = i.toString().split("");
-            if (inputString.includes("3")) {
-              numArray.push("Won't you be my neighbor?");
-            } else if (inputString.includes("2")) {
-                numArray.push("Boop!")
-            } else if (inputString.includes("1")){
-                numArray.push("Beep!")
-            } else {
-                numArray.push(inputString);
-            }
-            }
-        return numArray.join(", ")
+        numArray = [];
+        for (let i = 0; i <= numInput; i++) {
+        let inputString = i.toString().split('');
+        if (inputString.includes('3')) {
+            numArray.push("Won't you be my neighbor?");
+        } else if (inputString.includes('2')) {
+            numArray.push('Boop!');
+        } else if (inputString.includes('1')) {
+            numArray.push('Beep!');
+        } else {
+            numArray.push(inputString);
+        }
     }
-    document.getElementById("beep-boop").innerText = result
-
+        return numArray.join(', ');
+    }
+    return (document.getElementById('beep-boop').innerText = result);
 });
-    
-resetBtn.addEventListener("click", function() {
-    document.getElementById("roboger").reset();
-    document.getElementById("beep-boop").style.display = "none";
-    
+
+resetBtn.addEventListener('click', function () {
+    document.getElementById('roboger').reset();
+    submitButton.removeAttribute('disabled');
+    document.getElementById('beep-boop').innerText = '';
 });
 });
