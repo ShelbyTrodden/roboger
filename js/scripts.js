79d1df3c61568e11d5f6af3835c0c55d9f0f89b5
Shelby Trodden <shelby.trodden@gmail.com>
2022-07-09 23:51:34 -0500

    correctly link reset button

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
 // User Interface Logic
 window.addEventListener("load", function(event) {
 
-  
-     
+    const form = document.getElementById("roboger");
+    const resetBtn =  document.getElementById("reset");
     
-  form.addEventListener("submit", function() {
+form.addEventListener("submit", function(event) {
     event.preventDefault();
-  
-  
-    });
+
+    const numInput = parseInt(document.getElementById("number").value);
+
+    numIdentify(numInput); {
+        if (numInput.includes(1, 2, 3)); {
+            return true;
+    } return false; 
+    };  
+});
     
-  resetBtn.addEventListener("click", function() {
-       
-      });
+resetBtn.addEventListener("click", function() {
+    document.getElementById("roboger").reset();
+    
+});
     });
 
-numIdentify(number); {
-    if (number.includes(1, 2, 3)); {
-        return true;
-} return false; 
-};
