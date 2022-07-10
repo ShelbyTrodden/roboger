f930800cc707fd52e92525ba432faf56418e88b1
Shelby Trodden <shelby.trodden@gmail.com>
2022-07-10 01:19:45 -0500

    WIP working on functionality of loop and array, researching

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,18 @@ window.addEventListener("load", function(event) {
     
 form.addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log("get in loser");
+    
     const numInput = parseInt(document.getElementById("number").value);
-    console.log("were going shopping");
-    function numIdentify(numInput) {
-        if (numInput.includes("1", "2", "3")); {
-            return console.log("you cant sit with us");
-    }
-    };  
-});
+    
+  
+    
+
+    });  
     
 resetBtn.addEventListener("click", function() {
     document.getElementById("roboger").reset();
     
 });
-    });
+});
+
 
