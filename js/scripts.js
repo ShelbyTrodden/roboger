1b28b5b4f4ebf51d628a5fa9e1b777190dfdad09
Shelby Trodden <shelby.trodden@gmail.com>
2022-07-10 10:18:35 -0500

    make minor language tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/ User Interface Logic
+// User Interface Logic
 window.addEventListener('load', function () {
     const form = document.getElementById('roboger');
     const resetBtn = document.getElementById('reset');
@@ -27,7 +27,7 @@ form.addEventListener('submit', function (event) {
     }
         return numArray.join(', ');
     }
-    return (document.getElementById('beep-boop').innerText = result);
+    (document.getElementById('beep-boop').innerText = result);
 });
 
 resetBtn.addEventListener('click', function () {
