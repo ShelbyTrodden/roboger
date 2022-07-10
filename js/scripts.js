b587ff9ec3c8158358b250f928854c8ee20d719c
Shelby Trodden <shelby.trodden@gmail.com>
2022-07-09 23:06:17 -0500

    work on setting up html form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+// User Interface Logic
+window.addEventListener("load", function(event) {
+
+  
+     
+    
+  form.addEventListener("submit", function() {
+    event.preventDefault();
+  
+  
+    });
+    
+  resetBtn.addEventListener("click", function() {
+       
+      });
+    });
+
 numIdentify(number); {
     if (number.includes(1, 2, 3)); {
         return true;
