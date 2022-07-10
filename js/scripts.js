8d7783dc867c6bdcd1f18e6c7db6cf6e682989b7
Shelby Trodden <shelby.trodden@gmail.com>
2022-07-10 01:46:49 -0500

    working on numIdentify function

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,23 @@ form.addEventListener("submit", function(event) {
     
     const numInput = parseInt(document.getElementById("number").value);
     
-  
-    
+    function numIdentify(number) {
+        let newArray=[];
+            for (let i=0; i<number.length; i++){
+                newArray.push(i.toString());
+    }
+    console.log("you dont suck i guess")
 
-    });  
+    newArray.forEach(function(element,index){
+        if (element.includes(1)){
+            console.log("thats so fetch");
+        }
+    })
+    }; 
     
 resetBtn.addEventListener("click", function() {
     document.getElementById("roboger").reset();
     
 });
+
 });
-
-
